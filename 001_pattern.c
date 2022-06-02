ec512e125bf77df298c9d7b0a7769573f045e2f8
REM-moe <98999089+REM-moe@users.noreply.github.com>
2022-06-02 20:20:16 +0530

    Add files via upload

---
+++ 001_pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main() 
+/* you can also use int main() - void main () 
+used to remove unwated return 0; part in code
+ by making it simpler */
+
+// Program to print a simple welcome message //
+
+{
+	printf("\t welcome! \n");
+
+ // \n - new line | \t - tab space //
+ // no return 0; since we used void main instead of int main () //
+
+}
