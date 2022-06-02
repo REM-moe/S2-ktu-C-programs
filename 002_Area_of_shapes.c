51d193e3dd9a5d9f466951caf2ed9f2f3431c86c
REM-moe <98999089+REM-moe@users.noreply.github.com>
2022-06-02 20:39:57 +0530

    Update 002_Area_of_shapes.c

--- 002_Area_of_shapes.c
+++ 002_Area_of_shapes.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main ()
 {
-// Circle //
-	int r ; //variale to store radius user enters//
+// Circle (input any formula and change the shape accordingly )// 
+	int r ; //variale to store radius user enters| if diffrent shape require more parameters declare more variables //
 	printf("Enter radius of circle \n");
 	scanf("%d ",&r) ; 
 /* takes input from user '%f' - act a place holder for the float  to come into place  and %d for int 
@@ -13,7 +13,7 @@ void main ()
 	either make a seperate variable called area like this
 	before that you need to declare it so the code goes like
 	float area ;
-	area = r * r * 3.14;
+	area = r * r * 3.14; cange this line to any equation you know
 	and print area 
 	*/
 	
